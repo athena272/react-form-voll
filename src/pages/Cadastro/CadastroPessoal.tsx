ebfbf9cd6e259807d8b0d76f8af5ea28bf1707fc
athena272 <guilhermera272@gmail.com>
2024-07-09 04:08:31 -0300

    Update CadastroPessoal.tsx

--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -1,4 +1,4 @@
-import { Button, Label, Fieldset, Input, Form, Titulo } from "../../components";
+import { Button, Label, Fieldset, Input, Form, Titulo, ErrorMessage } from "../../components";
 import { useForm } from "react-hook-form";
 
 interface FormInputTipos {
@@ -10,11 +10,12 @@ interface FormInputTipos {
 }
 
 const CadastroPessoal = () => {
-  const { register, handleSubmit } = useForm<FormInputTipos>();
-
-  const onSubmitForm = (data: FormInputTipos) => {
-    console.log(data)
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = useForm<FormInputTipos>();
 
   function validarEmail(valor: string) {
     const formatoEmail = /^[^\s@]+@alura\.com\.br$/;
@@ -25,6 +26,19 @@ const CadastroPessoal = () => {
     return true;
   }
 
+  const senha = watch("senha")
+  const validaSenha = {
+    obrigatorio: (val: string) =>
+      !!val || "Por favor, insira a senha novamente",
+    tamanhoMinimo: (val: string) =>
+      val.length >= 6 || "A senha deve ter pelo menos 6 caracteres",
+    senhaIguais: (val: string) => val === senha || "As senhas não correspondem",
+  };
+
+  const onSubmitForm = (data: FormInputTipos) => {
+    console.log(data)
+  }
+  
   return (
     <>
       <Titulo>Insira alguns dados básicos:</Titulo>
@@ -35,11 +49,20 @@ const CadastroPessoal = () => {
             id="campo-nome"
             placeholder="Digite seu nome completo"
             type="text"
+            $error={!!errors.nome}
             {...register('nome', {
-              required: true,
-              minLength: 5
+              required: "Campo de nome é obrigatório",
+              minLength: {
+                value: 5,
+                message: "O nome deve ter pelo menos cinco caracteres",
+              },
             })}
           />
+          {errors.nome &&
+            <ErrorMessage>
+              {errors.nome.message}
+            </ErrorMessage>
+          }
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-email">E-mail</Label>
@@ -48,13 +71,17 @@ const CadastroPessoal = () => {
             placeholder="Insira seu endereço de email"
             type="email"
             {...register('email', {
-              required: true,
+              required: "Campo de e-mail é obrigatório",
               validate: validarEmail
               // validate: value => validarEmail(value)
             })}
           />
+          {errors.email &&
+            <ErrorMessage>
+              {errors.email.message}
+            </ErrorMessage>
+          }
         </Fieldset>
-
         <Fieldset>
           <Label>Telefone</Label>
           <Input
@@ -62,20 +89,38 @@ const CadastroPessoal = () => {
             type="text"
             placeholder="Ex: (DDD) XXXXX-XXXX"
             {...register('telefone', {
-              pattern: /^\(\d{2,3}\) \d{5}-\d{4}$/,
-              required: true,
+              pattern: {
+                value: /^\(\d{2,3}\) \d{5}-\d{4}$/,
+                message: "O telefone inserido está no formato incorreto",
+              },
+              required: "Campo de telefone é obrigatório",
             })}
           />
+          {errors.telefone &&
+            <ErrorMessage>
+              {errors.telefone.message}
+            </ErrorMessage>
+          }
         </Fieldset>
-
         <Fieldset>
           <Label htmlFor="campo-senha">Crie uma senha</Label>
           <Input
             id="campo-senha"
             placeholder="Crie uma senha"
             type="password"
-            {...register('senha')}
+            {...register('senha', {
+              required: "O campo de senha é obrigatório",
+              minLength: {
+                value: 6,
+                message: "A senha deve ter pelo menos seis caracteres",
+              },
+            })}
           />
+          {errors.senha &&
+            <ErrorMessage>
+              {errors.senha.message}
+            </ErrorMessage>
+          }
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-senha-confirmacao">Repita a senha</Label>
@@ -83,8 +128,16 @@ const CadastroPessoal = () => {
             id="campo-senha-confirmacao"
             placeholder="Repita a senha anterior"
             type="password"
-            {...register('senhaVerificada')}
+            {...register('senhaVerificada', {
+              required: "Repita a senha",
+              validate: validaSenha
+            })}
           />
+          {errors.senhaVerificada &&
+            <ErrorMessage>
+              {errors.senhaVerificada.message}
+            </ErrorMessage>
+          }
         </Fieldset>
         <Button type="submit">Avançar</Button>
       </Form>
